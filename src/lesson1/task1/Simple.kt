1e868eec3ed33d45b9ab584fdf8ee70be5dca425
semiletkin <semiletkinalex@gmail.com>
2020-05-02 15:50:55 +0300

    Решены задачи первого урока
     градусы в радианы перевести
     вычислить третью цифру в числе

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -51,6 +51,9 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
 fun main() {
     val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
     println("Root product: $x1x2")
+    val x = 7
+    val test = sqr(x)
+    println("My vyvodim kvadrat ot $x , on raven $test")
 }
 
 /**
@@ -76,7 +79,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double = ((deg + min / 60.0 + sec / 3600.0) * PI / 180.0)
 
 /**
  * Тривиальная
@@ -92,7 +95,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int = (number) % 1000 / 100
 
 /**
  * Простая
